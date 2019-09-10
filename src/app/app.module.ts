481cc8200061314c4d7f40380d06fdd8ae8d48c2
jesus escalante <jdescalantel@ul.edu.co>
2019-09-10 15:41:27 -0500

    parcial1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,19 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
 import { LoginComponent } from './login/login.component';
 import { ThemeComponent } from './theme/theme.component';
+import { AdministrationComponent } from './pages/administration/administration.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { UsersComponent } from './pages/administration/users/users.component';
+import { RolesComponent } from './pages/administration/roles/roles.component';
+import { CategoriesComponent } from './pages/settings/categories/categories.component';
+import { AuthorsComponent } from './pages/settings/authors/authors.component';
+import { AreasComponent } from './pages/settings/areas/areas.component';
+import { SectionsComponent } from './pages/settings/sections/sections.component';
+import { BooksComponent } from './pages/settings/books/books.component';
 
 @NgModule({
   declarations: [
-    AppComponent,LoginComponent, ThemeComponent
+    AppComponent,LoginComponent, ThemeComponent, AdministrationComponent, SettingsComponent, UsersComponent, RolesComponent, CategoriesComponent, AuthorsComponent, AreasComponent, SectionsComponent, BooksComponent
   ],
   imports: [
     BrowserModule,AppRoutingModule
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,6 +2,14 @@ import { NgModule } from "@angular/core";
 import {Routes,RouterModule, Router} from "@angular/router";
 import { LoginComponent } from './login/login.component';
 import { ThemeComponent } from './theme/theme.component';
+import { UsersComponent } from './pages/administration/users/users.component';
+import { RolesComponent } from './pages/administration/roles/roles.component';
+import { CategoriesComponent } from './pages/settings/categories/categories.component';
+import { AuthorsComponent } from './pages/settings/authors/authors.component';
+import { AreasComponent } from './pages/settings/areas/areas.component';
+import { SectionsComponent } from './pages/settings/sections/sections.component';
+import { BooksComponent } from './pages/settings/books/books.component';
+
 export const routes: Routes=[{
     path  : 'login',    
     component : LoginComponent
@@ -11,8 +19,29 @@ export const routes: Routes=[{
     component: ThemeComponent,
     children: [
         {
-            path: '',
+            path: 'dashboard',
             loadChildren:'./pages/dashboard/dashboard.module#DashboardModule'
+        },{
+            path  : 'users',    
+            component :  UsersComponent
+        },{
+            path  : 'roles',    
+            component :  RolesComponent
+        },{
+            path  : 'categories',    
+            component :  CategoriesComponent
+        },{
+            path  : 'authors',    
+            component :  AuthorsComponent
+        },{
+            path  : 'areas',    
+            component :  AreasComponent
+        },{
+            path  : 'sections',    
+            component :  SectionsComponent
+        },{
+            path  : 'books',    
+            component : BooksComponent
         }
     ]
 
