98a46dd71cda9b5a4c058490496e953a2a063f3b
jesus escalante <jdescalantel@ul.edu.co>
2019-09-22 19:18:51 -0500

    modificado

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,13 +2,7 @@ import { NgModule } from "@angular/core";
 import {Routes,RouterModule, Router} from "@angular/router";
 import { LoginComponent } from './login/login.component';
 import { ThemeComponent } from './theme/theme.component';
-import { UsersComponent } from './pages/administration/users/users.component';
-import { RolesComponent } from './pages/administration/roles/roles.component';
-import { CategoriesComponent } from './pages/settings/categories/categories.component';
-import { AuthorsComponent } from './pages/settings/authors/authors.component';
-import { AreasComponent } from './pages/settings/areas/areas.component';
-import { SectionsComponent } from './pages/settings/sections/sections.component';
-import { BooksComponent } from './pages/settings/books/books.component';
+
 
 export const routes: Routes=[{
     path  : 'login',    
@@ -19,29 +13,15 @@ export const routes: Routes=[{
     component: ThemeComponent,
     children: [
         {
-            path: 'dashboard',
+            path: '',
             loadChildren:'./pages/dashboard/dashboard.module#DashboardModule'
         },{
-            path  : 'users',    
-            component :  UsersComponent
-        },{
-            path  : 'roles',    
-            component :  RolesComponent
-        },{
-            path  : 'categories',    
-            component :  CategoriesComponent
-        },{
-            path  : 'authors',    
-            component :  AuthorsComponent
-        },{
-            path  : 'areas',    
-            component :  AreasComponent
-        },{
-            path  : 'sections',    
-            component :  SectionsComponent
-        },{
-            path  : 'books',    
-            component : BooksComponent
+            path  : '',    
+            loadChildren: './pages/administration/administration.module#AdministrationModule'
+        },
+        {
+            path  : '',    
+            loadChildren: './pages/settings/settings.module#SettingsModule'
         }
     ]
 
--- src/app/pages/settings/settings.module.ts
+++ src/app/pages/settings/settings.module.ts
@@ -5,13 +5,15 @@ import { AuthorsComponent } from './authors/authors.component';
 import { AreasComponent } from './areas/areas.component';
 import { SectionsComponent } from './sections/sections.component';
 import { BooksComponent } from './books/books.component';
+import { SettingsRoutingModule } from './settings-routing.module';
 
 
 
 @NgModule({
   declarations: [CategoriesComponent, AuthorsComponent, AreasComponent, SectionsComponent, BooksComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SettingsRoutingModule
   ]
 })
 export class SettingsModule { }
